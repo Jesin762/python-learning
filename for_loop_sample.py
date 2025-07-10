3db7eb98202c95d820cdd062bdc224a74f8fae86
jesin762 <clonpain@gmail.com>
2025-07-10 14:06:02 +0530

    for lopp basic programs

---
+++ for_loop_sample.py
@@ -0,0 +1,33 @@
+"""
+#list
+my_list=[56,77,78,99]
+for j in my_list:
+    print(j,end =" ")
+
+#string
+student_name=("jesin","shahid","rohit","aswin","zaman","athul")
+for i in student_name:
+    print(i)
+
+    
+
+#set
+num={44,77,88,12}
+for k in num:
+    print(k)
+    
+
+#dictonary
+h={"jesin":30,40:50,"kochi":60}
+#for d in h:
+  #  print(h.items())
+for k,val in h.items():
+    print(k,val)
+    
+box=[33,56,78,99]
+print(list(enumerate(box)))
+for c,k in enumerate(box):
+    print(c,k)
+    """
+
+
---
+++ range_pgm.py
@@ -0,0 +1,16 @@
+#print(list(range(5,50,5)))
+
+#for k in range(1,11):
+ #   print(k*2)
+
+"""
+num=int(input("enter your number"))
+for k in range(1,11):
+    print(F"{k} * {num}= {k*num}")
+"""
+
+num=int(input("enter your number"))
+for num in range(1,17):
+    if num%2==1:
+        print(num)
+    
