19b8d1ed2af921341986f1ed7cc9421d40153925
jesin762 <clonpain@gmail.com>
2025-07-11 13:49:53 +0530

    range program and workout program.py

--- range_pgm.py
+++ range_pgm.py
@@ -7,10 +7,26 @@
 num=int(input("enter your number"))
 for k in range(1,11):
     print(F"{k} * {num}= {k*num}")
-"""
 
 num=int(input("enter your number"))
 for num in range(1,17):
     if num%2==1:
         print(num)
     
+fruits=("Apple","orange","avacado","lemon","watermelon")
+for fruit in fruits:
+     if fruit.startswith("a"):
+          print(fruit)
+
+          
+fruits=("Apple","orange","avacado","lemon","watermelon")          
+for f in fruits:
+    if f.lower()[0]=='a':
+       print(f)
+       """
+#vowels
+country=["argentina","france","europe"]
+for c in country:
+    if c.lower()[0] in "aeiou":
+        print(c)
+
---
+++ work_out.py
@@ -0,0 +1,16 @@
+"""
+for num in range(1,11):
+    if num==3:
+        continue
+    print(num)
+    """
+
+"""for num in range(1,11):
+    if num==7:
+        break
+    print(num)"""
+
+for r in range(1,4):
+    for j in range (1,r + 1):
+        print(j,end=" ")
+    print("")
