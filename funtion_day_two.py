4f0033714031527a81bbde0ca0a601c546cdccca
jesin762 <clonpain@gmail.com>
2025-07-17 13:51:32 +0530

    funtion day 2 recursion and workout modified

---
+++ funtion_day_two.py
@@ -0,0 +1,29 @@
+# ---- *args and **kwargs ----
+ #tuple
+
+"""def total(*n):
+    print(sum(n))  
+total(56,77,88)"""
+
+#dictinoray
+
+"""def greetings(**student):    
+    print(f"hello {student["name"]} you are {student["age"]} years old")
+greetings(age=19,name="jesin")"""
+#
+
+"""def a(a,b,):
+    return a,b
+c,b=a(7,3)
+print(c,b)"""
+  #global keyword
+
+"""c=0
+def sample():
+    global c
+    c=c+2         
+sample()
+print(c)"""
+
+
+
--- work_out_pattern.py
+++ work_out_pattern.py
@@ -11,7 +11,12 @@ for i in range(5,0,-1):            # q no:2
 """for i in range(5,0,-1):       # q no: 2
     print(" "*int(5-i),"* "*i) """
 
-for i in range(1,6):
-    for k in range(1,i+1):
-        print(i*k,end=" ")
-    print()
+for row in range(1,5):
+    for col in range(1,5):
+        if (col==1 or col==4) or (row==1 or row==4):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()        
+
+        
