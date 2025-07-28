23b171a6fe6c296890215ac6e961fe1d87887671
jesin762 <clonpain@gmail.com>
2025-07-28 12:20:34 +0530

    updated project

--- first_project.py
+++ first_project.py
@@ -1,10 +1,29 @@
+a=[ ]
 while True:
     print("==to do list==\n" "1.add task\n" "2.show task\n" "3.mark task as done\n" "4.exit")
     choice=int(input("enter your choice :"))
     if choice==1:
-        choice1=int(input("how many task you want to add:"))
-        choice2=str(input("enter your task:"))
-        print("task is added")
+            choice1=int(input("how many task you want to add:"))
+            print(choice1)
+            for i in range(choice): 
+                 c=input("enter your task:")
+                 print(c)
+                 print("task added")
+                 a.append(choice)
+    elif choice==2:
+          print()
+          for c in a:
+           print(c)
+           
+
+         
+
+         
+
+ 
+         
+    
+
        
             
               
